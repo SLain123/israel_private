80c020f3945d43bc634ecb232edaf282608df285
SLain <sl163@ya.ru>
2020-06-02 18:21:17 +0300

    Дефолтная структура проекта gulp под less без автопрописывания шрифтов.

---
+++ gulpfile.js
@@ -0,0 +1,164 @@
+let project_folder = require('path').basename(__dirname + '_public');
+let source_folder = '#src';
+
+let path = {
+  build: {
+    html: project_folder + '/',
+    css: project_folder + '/css/',
+    js: project_folder + '/js/',
+    img: project_folder + '/img/',
+    fonts: project_folder + '/fonts/'
+  },
+  src: {
+    html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
+    css: source_folder + '/less/style.less',
+    js: source_folder + '/js/script.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    fonts: source_folder + '/fonts/*.ttf'
+  },
+  watch: {
+    html: source_folder + '/**/*.html',
+    css: source_folder + '/less/**/*.less',
+    js: source_folder + '/js/**/*.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}'
+  },
+  clean: './' + project_folder + '/'
+};
+
+let {src, dest} = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  fileinclude = require('gulp-file-include'),
+  del = require('del'),
+  less = require('gulp-less'),
+  autoprefixer = require('gulp-autoprefixer'),
+  gcmq = require('gulp-group-css-media-queries'),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify-es').default,
+  babel = require('gulp-babel'),
+  imageMin = require('gulp-imagemin'),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html'),
+  webpcss = require('gulp-webpcss'),
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2');
+
+
+function browserSync(params) {
+    browsersync.init({
+        server: {
+            baseDir: "./" + project_folder + '/'
+        },
+        port: 3000,
+        notify: false
+  });
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
+}
+
+function css() {
+  return src(path.src.css)
+    .pipe(
+      less({
+        outputStyle: 'expanded'
+      })
+    )
+    .pipe(gcmq())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 5 versions'],
+        cascade: true
+      })
+    )
+    .pipe(webpcss())
+    .pipe(dest(path.build.css))
+    .pipe(cleanCss())
+    .pipe(
+      rename({
+        extname: '.min.css'
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+      uglify()
+    )
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(
+      rename({
+        extname: '.min.js'
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp()
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imageMin({
+        interlaced: true,
+        progressive: true,
+        optimizationLevel: 5,
+        svgoPlugins: [
+            {
+                removeViewBox: true
+            }
+        ]
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+function fonts() {
+  src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts))
+  return src(path.src.fonts)
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts))
+}
+
+function watchFiles(param) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+}
+
+function clean(param) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
