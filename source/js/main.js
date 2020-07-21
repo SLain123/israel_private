a15171fc7f83446c45da6e4fb47458bdfd8439fb
SLain <sl163@ya.ru>
2020-07-21 20:21:40 +0300

    Добавляет готовый блок с программами;

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,22 @@
 "use strict";
 
-import {
-  polyfills
-} from "./utils/polyfills";
+import {polyfills} from "./utils/polyfills";
 
 // Modules
-//----------------------------------
+// ----------------------------------
 
 import modalWindow from "./modules/modal-window"; // Скрипт открытия и закрытия модального окна;
 import phoneMask from "./modules/phone-mask"; // Скрипт для маски в поле ввода телефона;
 import validateAndSendForm from "./modules/validate&send-form"; // Скрипт проверки валидности полей формы и отправки данных в localstorage;
 import scroll from "./modules/scroll"; // Скрипт нижнего скролла для кнопки на главной странице;
 import swiper from "./vendor/swiper"; // Подключение свайпера;
-import {
-  mySwiper
-} from "./modules/tabs";
+import {mySwiper, tabs} from "./modules/tabs"; // Инициализация свайпера и подключение табов;
 
 modalWindow();
 phoneMask();
 validateAndSendForm();
 scroll();
+tabs();
 
 // Utils
 // ---------------------------------
--- source/js/modules/tabs.js
+++ source/js/modules/tabs.js
@@ -1,34 +1,95 @@
 let mySwiper = new Swiper(".swiper-container", {
-
+  direction: "horizontal",
+  slidesPerView: 1.8,
+  spaceBetween: 10,
+  freeMode: true,
   breakpoints: {
-    // when window width is >= 768px
-      320: {
-        direction: "horizontal",
-        slidesPerView: 1.8,
-        spaceBetween: 10,
-        freeMode: true,
-      },
-      769: {
-        direction: "vertical",
-      }
-
-    }
+    769: {
+      direction: "vertical",
+    },
+  },
 });
 
-const allTabBtn = document.querySelectorAll(".programs__swiper-slide");
-
-const getHover = function() {
-  let elem = this;
-  let img = elem.querySelector('picture');
-  console.log(img);
+if (window.innerWidth > 769) {
+  mySwiper.destroy();
 }
 
-for(let btn of allTabBtn) {
-  btn.addEventListener('click', getHover);
+function tabs() {
+  const allTabBtn = document.querySelectorAll(".programs__swiper-slide");
+  const allInfoBlocks = document.querySelectorAll(".programs__info-container");
+
+  // Функции добавления или удаления классов отвечающих за ховер иконок и текста кнопок
+
+  const addHover = function () {
+    let icon = this.querySelector(".programs__swiper-icon");
+    let header = this.querySelector("span");
+
+    if (!this.classList.contains("programs__swiper-slide_active")) {
+      icon.classList.add("programs__swiper-icon_hover-style");
+      header.classList.add("programs__swiper-text_hover-style");
+    }
+  };
+
+  const removeHover = function () {
+    let icon = this.querySelector(".programs__swiper-icon");
+    let header = this.querySelector("span");
+
+    icon.classList.remove("programs__swiper-icon_hover-style");
+    header.classList.remove("programs__swiper-text_hover-style");
+  };
+
+  // Функции отвечающие за добавление или удаление классов отвечающих за активацию кнопок;
+
+  const addActive = function (elem) {
+    let icon = elem.querySelector(".programs__swiper-icon");
+    let header = elem.querySelector("span");
+
+    removeActive();
+
+    elem.classList.add("programs__swiper-slide_active");
+    icon.classList.add("programs__swiper-icon_active-style");
+    header.classList.remove("programs__swiper-text_hover-style");
+  };
+
+  const removeActive = function () {
+    for (let btn of allTabBtn) {
+      let icon = btn.querySelector(".programs__swiper-icon");
+
+      btn.classList.remove("programs__swiper-slide_active");
+      icon.classList.remove("programs__swiper-icon_active-style");
+    }
+  };
+
+  // Функция отображения и скрытия информационного окна
+
+  const displayTab = function (num) {
+    hideAllTabs();
+
+    allInfoBlocks[num].classList.remove("programs__info-container_hide");
+  };
+
+  const hideAllTabs = function () {
+    for (let tab of allInfoBlocks) {
+      tab.classList.add("programs__info-container_hide");
+    }
+  };
+
+  // События различных ивентов с кнопками
+
+  for (let btn of allTabBtn) {
+    btn.addEventListener("mouseenter", addHover);
+  }
+
+  for (let btn of allTabBtn) {
+    btn.addEventListener("mouseleave", removeHover);
+  }
+
+  for (let i = 0; i < allTabBtn.length; i++) {
+    allTabBtn[i].addEventListener("click", function () {
+      addActive(allTabBtn[i]);
+      displayTab(i);
+    });
+  }
 }
 
-
-export {
-  mySwiper
-};
-
+export { mySwiper, tabs };
